1b5e5da8a90f6caf5b19b9f0a412f18689090b3b
firdavsokilov <frdavsokilov04@gmail.com>
2025-01-05 20:49:29 +0500

    Debugging

--- server/index.js
+++ server/index.js
@@ -4,10 +4,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://ilearning-mm6o.onrender.com',
-    credentials: true,
-}));
+app.use(cors());
 
 // API Routes
 app.use('/auth', require('./routes/auth'));
@@ -16,17 +13,8 @@ app.use('/api/templates', require('./routes/templates'));
 app.use('/api/questions', require('./routes/questions'));
 app.use('/api/likes', require('./routes/likes'));
 app.use('/api/comments', require('./routes/comments'));
+app.use('/api/answers', require('./routes/answers'));
 
-// Serve static files from React app if the build directory exists
-const reactBuildPath = path.join(__dirname, '../client/build');
-if (require('fs').existsSync(reactBuildPath)) {
-    app.use(express.static(reactBuildPath));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(reactBuildPath, 'index.html'));
-    });
-} else {
-    console.log('React build directory not found. Skipping static file serving.');
-}
 
 // Start server
 const PORT = process.env.PORT || 10000;
